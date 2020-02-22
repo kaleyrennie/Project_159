f27d58d0556d12463132b0a4aa1bdbc62d7b07b2
Kaley <kaleyrennie14@gmail.com>
2020-02-21 21:01:43 -0800

    Add project files.

---
+++ Project_159/Addition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_159
+{
+    class Addition
+    {
+        //1.
+        public int MathAdd(int x)
+        {
+            x = x + 2;
+            return x;
+        }
+
+        //2. 4.
+        public int MathAdd(int x) //take in int and show same int to screen & take in decimal and display as decimal
+        {
+            return x;
+        }
+
+        //3.6.
+        public decimal MathAdd(decimal y) //take in a decimal and convert answer to int
+        {
+            y = y + 2;
+            decimal newVar1 = Convert.ToInt32(y);
+            return y;
+        }
+        //5.
+        public string MathAdd(string z) //take in a string and convert answer to int
+        {
+            var newVar = Convert.ToInt32(z);
+        }
+
+    }
+}
---
+++ Project_159/Program.cs
@@ -0,0 +1,47 @@
+﻿using Assignment_159;
+using System;
+
+namespace Assignment_159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition objectAddition = new Addition();
+            decimal newVar1;
+            string newVar;
+
+            //1.
+            Console.WriteLine("Enter number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            //2.
+            Console.WriteLine("Enter number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            //3.
+            Console.WriteLine("Enter decimal:");
+            decimal y = Convert.ToDecimal(Console.ReadLine());
+
+            //4.
+            Console.WriteLine("Enter decimal:");
+            decimal y = Convert.ToDecimal(Console.ReadLine());
+
+            //5.
+            Console.WriteLine("Enter string:");
+            string z = Convert.ToString(Console.WriteLine());
+
+            //6. passes a string and then shows and int 
+
+
+
+
+
+
+        }
+
+    }
+}
+
+
+
